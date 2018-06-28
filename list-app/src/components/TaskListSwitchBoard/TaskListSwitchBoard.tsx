ef6daad02a57c6e42ce0c63f66797eebbac87426
Zhanpeng Liang <hectorilles@gmail.com>
2018-06-27 20:43:37 -0700

    feature: week slide

--- list-app/src/components/TaskListSwitchBoard/TaskListSwitchBoard.tsx
+++ list-app/src/components/TaskListSwitchBoard/TaskListSwitchBoard.tsx
@@ -13,9 +13,34 @@ export interface Props {
   onWeekChange?: (weekToActivate: PrevCurNextKey) => void;
 }
 
+interface WeekNeighbors {
+  left: PrevCurNextKey | null
+  right: PrevCurNextKey | null
+}
+
+type WeekBearing = {
+  [position in PrevCurNextKey]: WeekNeighbors
+}
+
+const weekBearing: WeekBearing = {
+  'prev': {
+    left: null,
+    right: 'cur',
+  },
+  'cur': {
+    left: 'prev',
+    right: 'next'
+  },
+  'next': {
+    left: 'cur',
+    right: null
+  }
+};
+
 // TODO: add slide feature to switch prev/cur/next week
 // activeWeek should never be undefined
-function TaskListSwitchBoard({ day, activeWeek = 'cur', taskListOfWeeks, currentTime }: Props) {
+function TaskListSwitchBoard({ day, activeWeek = 'cur', taskListOfWeeks,
+  currentTime, onWeekChange }: Props) {
   if (!taskListOfWeeks) {
     throw new Error("TaskListSwitchBoard: three lists of tasks not existing");
   }
@@ -26,9 +51,21 @@ function TaskListSwitchBoard({ day, activeWeek = 'cur', taskListOfWeeks, current
   // TODO: add one more layer to handle unfocused views
 
   const dateString = toMonthDateString(calculateRelativeDate(day, currentTime!));
+  const leftCard = weekBearing[activeWeek].left;
+  const rightCard = weekBearing[activeWeek].right;
+  const handleWeekSlideLeft = () => onWeekChange!(leftCard!);
+  const handleWeekSlideRight = () => onWeekChange!(rightCard!);
   return (
     <div onClick={seizeClickBubbling}>
       <div className="right"> {dateString} </div>
+      <div>
+        <button disabled={!leftCard} onClick={handleWeekSlideLeft}>
+          <i className="material-icons">keyboard_arrow_left</i>
+        </button>
+        <button disabled={!rightCard} onClick={handleWeekSlideRight}>
+          <i className="material-icons">keyboard_arrow_right</i>
+        </button>
+      </div>
       {
         !!dayTaskList ?
           <TaskList day={day} week={activeWeek}
